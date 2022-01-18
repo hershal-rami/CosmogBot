e4d00d77d46a07876a4e74489036d893638296a5
Hershal Rami <hershalrami@gmail.com>
2022-01-18 01:56:31 -0500

    added basic message response to the bot

--- bot.py
+++ bot.py
@@ -1,13 +1,35 @@
+import logging
 import os
 
 import discord
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+# Basic logging info outputs to discord.log file
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
+# Discord token set locally for security
+TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if ":scepthink:" in message.content.lower():
+        await message.channel.send(":scepthink:")
+    
+    if ":thonk:" in message.content.lower():
+        await message.channel.send(":thonk:")
+
+    if message.content.lower.startswith('hello'):
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
