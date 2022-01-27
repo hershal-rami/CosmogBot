b105bd59947f61bd9a7042a160912a795bddd01d
Hershal Rami <hershalrami@gmail.com>
2022-01-26 23:36:32 -0500

    Fixed datetime error in gm response

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import random
 import os
-import datetime
+from datetime import datetime
 
 import discord
 from discord.ext import commands
@@ -42,19 +42,27 @@ async def responder(message):
     # Fun responses: emoji responses, reactions, and hello message
     if ":scepthink:" in message.content.lower():
         await message.channel.send("<:scepthink:932156902555648101>") # must use emoji id    
+
     if ":thonk:" in message.content.lower():
         await message.channel.send("<:thonk:932152527061913640>")
+
     if ":copium:" in message.content.lower():
         await message.channel.send("<:copium:932152187616895006>")
+
     if ":sdn:" in message.content.lower():
         await message.channel.send("<:sdn:932156913012047922>")
+
     if ":cosmug:" in message.content.lower():
         await message.channel.send("<:cosmug:932895668450787329>")
+
     if "cosmog" in message.content.lower():
         await message.add_reaction("<:cosmug:932895668450787329>")
+
     if any(x in message.content.lower().split(" ") for x in ('hello', 'hi', 'hey', 'hiya')):
         await message.channel.send(random.choice(['Pepew! *(Hello!)*', 'Pepew! *(Hi!)*', 'Pepew! *(Heya!)*']))
-    if datetime.datetime.now().time().hour() <= 14 and any(x in message.content.lower() for x in ('gm', 'mornin')):
+
+    # only respond if gm is sent before 2pm
+    if datetime.now().hour <= 14 and any(x in message.content.lower() for x in ('gm', 'mornin')):
         await message.channel.send("Pe-pepew! *(Good morning!)*")
         await message.channel.send(file=discord.File('gm.gif'))
 
@@ -64,6 +72,7 @@ async def use_splash(ctx):
     await ctx.send("Cosmog used splash!")
     await asyncio.sleep(1) # dont use time.sleep, causes blocking
     await ctx.send("But nothing happened!")
+
 
 @bot.command(name='teleport', help='Cosmog\'s second best attack!')
 async def use_teleport(ctx):
@@ -83,36 +92,51 @@ async def print_standings(ctx):
     await ctx.send('```' + getFormattedStandings() + '```')
 
 
-# @bot.command(name='createteam', help='Usage: .createteam @User, Hex, Team Name')
-# async def createteam(ctx, *, message):
-#     # split message into individual arguments and format
-#     args = message.split(',')
+@bot.command(name='createteam', help='Usage: .createteam @User, Hex, Team Name')
+@commands.has_permissions(manage_roles=True)
+async def createteam(ctx, *, message):
+    # TODO if not mod then throw error
+    # insufficient role/perms
 
-#     if len(args) != 3:
-#         await ctx.send("error msg")
-#         raise commands.MissingRequiredArgument
+    # split message into individual arguments and format
+    args = message.split(',')
 
-#     user_id = args[0].strip()
+    if len(args) < 3:
+        #TODO fix error handling
+        await ctx.send("error msg")
+        raise commands.MissingRequiredArgument
+
+    # TODO error handling on incorrect format (no commas)
+
+    user_id = args[0].strip()
+    hex_code = args[1].strip()
+
+    # remove only leading space
+    team_name = args[2]
+    if team_name[0] == ' ':
+        team_name = team_name[1::]
     
-#     hex_code = args[1].strip()
+    duplicate = discord.utils.get(ctx.message.guild.roles, name=team_name)
+    if duplicate is not None:
+        # TODO throw error if role already exists
+        print("error team name already exists")
 
-#     # remove only leading space
-#     team_name = args[2]
-#     if team_name[0] == ' ':
-#         team_name = team_name[1::]
+    # TODO fix color of role
+    perms=discord.Permissions(administrator=True)
+    await ctx.message.guild.create_role(name=team_name, permissions=perms)
+    
+    # TODO remove No Team role from user using id
+    # TODO add newly created role to user using id
 
-#     print("test")
-#     await bot.create_rule(ctx.)
-#     duplicate = discord.utils.get(ctx.message.server.roles, name=team_name)
-#     #print(duplicate)
+    # TODO await ctx.send('Team $teamname has been created for Coach {user}!')
 
-#     await ctx.send('check console dumbass')
+    await ctx.send('role made')
 
 
-# @createteam.error
-# async def on_command_error(error, ctx):
-#     if isinstance(error, commands.MissingRequiredArgument):
-#         await ctx.send("Incorrect/missing arguments. Format should be '.createteam @User, Hex, Team Name") 
+@createteam.error
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Incorrect/missing arguments. Format should be '.createteam @User, Hex, Team Name") 
 
 # Go, Cosmog!
 bot.run(TOKEN)
